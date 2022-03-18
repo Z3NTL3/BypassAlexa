4802c4ff6d64ca307b7671b7e8126e2f501aed26
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2022-03-18 15:40:20 +0100

    Create bypass.py

---
+++ bypass.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+HEADER = {
+    'cache-control': 'no-cache',
+    'connection': 'keep-alive'
+}
+
+
+try:
+    sys.argv[1]
+except:
+    exit(0)
+
+if 'http://' in str(sys.argv[1]) or 'https://' in str(sys.argv[1]):
+    from sys import exit
+    exit(0)
+else:
+    pass
+
+class Alexa_Browser():
+    def __init__(self):
+        self.scraper = requests.get(f"https://www.alexa.com/siteinfo/{sys.argv[1]}", headers=HEADER)
+        self.contentscraper = self.scraper.content.decode('utf-8')
+
+    def AlexaRanking(self):
+        content_scraped = self.contentscraper
+        htmlsoup = BeautifulSoup(content_scraped,'html.parser')
+        ranking = htmlsoup.find('p', class_="big data").prettify()
+        rankstr = str(ranking).strip('')
+        getem = rankstr.split('\n')
+        return getem[4].strip(' ')
